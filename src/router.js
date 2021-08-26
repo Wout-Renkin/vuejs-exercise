a1bfb723d1ccb2a4be3c8d8669f5e4d039c7ae8f
Wout Renkin <r0631168@student.thomasmore.be>
2021-08-26 20:52:18 +0200

    first commit

---
+++ src/router.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from "vue-router";
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import RequestsReceived from "./pages/requests/RequestsReceived.vue";
+import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+import NotFound from "./pages/NotFound.vue";
+import CoachDetail from "./pages/coaches/CoachDetail.vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', redirect: '/coaches'},
+        {path: '/coaches', component: CoachesList},
+        {path: '/coaches/:id', component: CoachDetail, children: [
+            {path: 'contact', component: ContactCoach} // /coaches/c1/contant
+        ]},
+        {path: '/register', component: CoachRegistration},
+        {path: '/requests', component: RequestsReceived},
+        {path: '/:notFound(.*)', component: NotFound}
+
+    ],
+});
+
+
+export default router;
